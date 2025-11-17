c44819406c9dc9f5f229d6f269b4d659c61f3ceb
Yves De Saedeleer <ydsdsy@gmail.com>
2025-11-18 00:08:54 +0100

    Change ensureActive to coroutineContext.ensureActive

--- app/src/main/java/com/yvesds/vt5/features/speech/helpers/HeavyPathMatcher.kt
+++ app/src/main/java/com/yvesds/vt5/features/speech/helpers/HeavyPathMatcher.kt
@@ -11,6 +11,7 @@ import com.yvesds.vt5.features.speech.MatchResult
 import com.yvesds.vt5.utils.TextUtils
 import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.withTimeoutOrNull
+import kotlin.coroutines.coroutineContext
 
 /**
  * HeavyPathMatcher
@@ -46,7 +47,7 @@ class HeavyPathMatcher(
         matchContext: MatchContext,
         asrWeight: Double = DEFAULT_ASR_WEIGHT
     ): HeavyMatchResult? {
-        ensureActive()
+        coroutineContext.ensureActive()
 
         val normalized = TextUtils.normalizeLowerNoDiacritics(hypothesis)
         if (normalized.isBlank()) return null
@@ -86,7 +87,7 @@ class HeavyPathMatcher(
      * Try quick exact match for lower-priority hypotheses
      * Useful for remaining hypotheses beyond heavy-path count
      */
-    fun tryQuickExactMatch(
+    suspend fun tryQuickExactMatch(
         hypothesis: String,
         matchContext: MatchContext
     ): MatchResult? {
